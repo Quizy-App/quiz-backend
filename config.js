8bf83c0aec8efca2f6750a7ce552f9eb1ad91128
abhishek-develops <abhishek@careerstudio.net>
2022-05-18 12:32:50 +0530

    Mongo URI hidden

--- config.js
+++ config.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://Abhishek12:abhi_123@cluster0.7aoyd.mongodb.net/Quiz_DB?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB Connected");
   } catch (error) {
     console.log(error);
